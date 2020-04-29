9a69e5054b55bbf44715c27328276c0fa5e601ab
Craig Wilkinson <c.a.wilkinson1@googlemail.com>
2020-04-29 10:17:32 +0100

    Minor changes around the init.cmd
    Changed init.cmd to move the minecraft installer to a new folder, since the folder must be writable.  The java silent installer is not working and nor is the eula.txt for the minecraft server, need to look in to these issues before merging back with main.

--- Minecraft In Sandbox/install_minecraft_sandbox_on_host.ps1
+++ Minecraft In Sandbox/install_minecraft_sandbox_on_host.ps1
@@ -15,12 +15,31 @@ function CreateLogonScript
     Write-Verbose 'Creating init command...'
     $working_dir = "$env:USERPROFILE\minecraft_conf";
     $logon_cmd = "$working_dir\init.cmd"
+    $minecraft_cmd = "$working_dir\minecraft.cmd"
     $wdg_install_dir = 'C:\users\wdagutilityaccount\desktop\minecraft_conf'
     Write-Verbose "Saved logon script to $logon_cmd, this will be run upon starting Sandbox."
     New-Item -Force -Path $logon_cmd -ItemType File | Out-Null;
-    # TODO: INSTALL JAVA HERE!!
     Set-Content -Path $logon_cmd -Value @"
-java -Xmx1024M -Xms1024M -jar $wdg_install_dir\$minecraft_installer nogui
+cd "C:\Program Files"
+md minecraft
+xcopy $wdg_install_dir\$minecraft_installer "C:\Program Files\minecraft\$minecraft_installer"* /y /r /f
+start $wdg_install_dir\$java_installer /s ADDLOCAL="ToolsFeature,SourceFeature,PublicjreFeature"
+goto WAITLOOP
+:WAITLOOP
+if exist "C:\ProgramData\Oracle\Java\" goto INSTALLCOMPLETE
+ping -n 6 127.0.0.1 > nul
+goto WAITLOOP
+:INSTALLCOMPLETE
+start $wdg_install_dir\minecraft.cmd
+"@;
+    Write-Verbose "Saved minecraft server start to $minecraft_cmd, this will be run post java installation."
+    New-Item -Force -Path $minecraft_cmd -ItemType File | Out-Null;
+    Set-Content -Path $minecraft_cmd -Value @"
+cd "C:\Program Files\minecraft"
+echo eula=true > eula.txt
+md logs
+java -Xmx1024M -Xms1024M -jar "C:\Program Files\minecraft\$minecraft_installer" nogui
+pause
 "@;
     # Create the Sandbox configuration file with the new working dir & LogonCommand.
     $sandbox_conf = "$working_dir\minecraft_sandbox.wsb";
